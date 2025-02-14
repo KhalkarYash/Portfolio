338899536674d270b3c4b3cb6eb091946b51b568
KhalkarYash <yashmk2004@gmail.com>
2025-02-15 01:11:40 +0530

    Add BingeBrain project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,20 @@
 const ProjectCard = ({ prData }) => {
-  const { image, name, description, githubLink, techStack } = prData;
+  const { image, name, description, githubLink, techStack, liveLink } = prData;
   return (
     <div className="project-card">
       <img src={image} alt={name} />
       <h2>{name}</h2>
       <p>{description}</p>
-      <a href={githubLink}>
-        <button>GitHub Link</button>
-      </a>
+      <div className="projBtnCont">
+        {liveLink && (
+          <a href={liveLink} target="_blank">
+            <button>Live Link</button>
+          </a>
+        )}
+        <a href={githubLink} target="_blank">
+          <button>GitHub Link</button>
+        </a>
+      </div>
       <div className="techStack">
         {techStack.length > 0 ? (
           techStack.map((tech, index) => (
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,14 +4,34 @@ import collegeMarkTracker from "../../public/collegeMarkTracker.jpg";
 import movieRecommendationProject from "../../public/movieRecommendationProject.jpg";
 import namasteFood from "../../public/namasteFood.jpg";
 import todoProject from "../../public/todoProject.jpg";
+import bingeBrain from "../../public/bingeBrain.jpg";
+
+// Example of project object
+
+// {
+//   image: image Import name,
+//   name: "Project Name",
+//   description: "Project Description",
+//   githubLink: "Github Link",
+//   techStack: ["TechStack1","TechStack2",..."TechStackN"] Array of Tech Stack,
+//   liveLink: "Link of the project if is live",
+// }
 
 export const projects = [
   {
-    image: namasteFood,
-    name: "Namaste Food",
-    description: "Online Food Ordering Site.",
-    githubLink: "https://github.com/KhalkarYash/NamasteReact",
-    techStack: ["ReactJS", "JavaScript"],
+    image: bingeBrain,
+    name: "BingeBrain",
+    description: "Netflix and chill, but with a twist of AI.",
+    githubLink: "https://github.com/KhalkarYash/netflix-gpt-react",
+    techStack: [
+      "ReactJS",
+      "Firebase",
+      "React Redux",
+      "Redux Toolkit",
+      "GeminiAI",
+      "TailwindCSS",
+    ],
+    liveLink:"https://bingebrain.onrender.com/"
   },
   {
     image: collegeMarkTracker,
@@ -20,22 +40,29 @@ export const projects = [
       "A web application designed to help students track their academic performance and manage their grades effectively.",
     githubLink: "https://github.com/KhalkarYash/college-mark-tracker",
     techStack: ["Django", "BootStrap"],
+    liveLink: "https://college-mark-tracker.onrender.com/",
   },
   {
-    image: todoProject,
-    name: "todo List",
-    description:
-      "A comprehensive web development project demonstrating proficiency in HTML, CSS, and JavaScript.",
-    githubLink: "https://github.com/KhalkarYash/Final-Project-Web-Dev",
-    techStack: ["HTML", "CSS", "JavaScript"],
+    image: namasteFood,
+    name: "Namaste Food",
+    description: "Online Food Ordering Site.",
+    githubLink: "https://github.com/KhalkarYash/Namaste-Food-React",
+    techStack: [
+      "ReactJS",
+      "React Redux",
+      "Redux Toolkit",
+      "TailwindCSS",
+      "Jest",
+    ],
+    liveLink: "https://khalkaryash.github.io/Namaste-Food-React/",
   },
   {
-    image: alarmClock,
-    name: "Alarm Clock",
-    description:
-      "A functional alarm clock application built using JavaScript to set and manage alarms.",
-    githubLink: "https://github.com/KhalkarYash/AlarmClock",
-    techStack: ["HTML", "CSS", "JavaScript"],
+    image: movieRecommendationProject,
+    name: "Movie Recommendation System",
+    description: "A web app to recommend movies based on genres.",
+    githubLink: "https://github.com/KhalkarYash/Movie-Recommendation-System",
+    techStack: ["HTML", "CSS", "JavaScript", "TMDb API"],
+    liveLink: "https://khalkaryash.github.io/Movie-Recommendation-System/",
   },
   {
     image: blogProject,
@@ -44,12 +71,24 @@ export const projects = [
       "A blogging platform developed as part of web development training, allowing users to create and manage blog posts.",
     githubLink: "https://github.com/KhalkarYash/Blog-Project-Web-Training",
     techStack: ["HTML", "CSS", "JavaScript"],
+    liveLink: "https://khalkaryash.github.io/Blog-Project-Web-Training/",
   },
   {
-    image: movieRecommendationProject,
-    name: "Movie Recommendation System",
-    description: "A web app to recommend movies based on genres.",
-    githubLink: "https://github.com/KhalkarYash/Movie-Recommendation-System",
-    techStack: ["HTML", "CSS", "JavaScript", "TMDb API"],
+    image: alarmClock,
+    name: "Alarm Clock",
+    description:
+      "A functional alarm clock application built using JavaScript to set and manage alarms.",
+    githubLink: "https://github.com/KhalkarYash/AlarmClock",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    liveLink: "https://khalkaryash.github.io/AlarmClock/",
+  },
+  {
+    image: todoProject,
+    name: "todo List",
+    description:
+      "A comprehensive web development project demonstrating proficiency in HTML, CSS, and JavaScript.",
+    githubLink: "https://github.com/KhalkarYash/Final-Project-Web-Dev",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    liveLink: "https://khalkaryash.github.io/Final-Project-Web-Dev/",
   },
 ];
